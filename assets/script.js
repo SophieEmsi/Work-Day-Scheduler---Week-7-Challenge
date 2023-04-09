0d4ce9f9d13b27ea7f72b5310141ab763f8eddc9
Sophie McNally <soph_mcnally@msn.com>
2023-04-09 18:55:34 +0100

    adding todays date to the page using moment.js

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var todayDate = moment().format('MMMM Do YYYY');
+
+// document.getElementById('currentDay').innerHTML = todayDate; // i wrote the code firstly in the long js
+
+$('#currentDay').html(todayDate);
+
+
+
+
+
