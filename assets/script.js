e2469787913db9b948f233c0d406e00d0be44fa6
Sophie McNally <soph_mcnally@msn.com>
2023-04-10 18:37:25 +0100

    saving the text to the local storage and keeping it on page when refreshing

--- assets/script.js
+++ assets/script.js
@@ -37,3 +37,27 @@ function checkTime() { // new function to check the time and display the correct
     });
   });
 
+// saving the text input to local storage
+
+$(document).ready(function(){ // execure new function when browser is ready
+
+    $(".time-block").each(function() { // select all the class elements called time-block and run through each in a new function 
+        var rowId = $(this).attr('id'); // take value of id and assign it to the rowId variable
+        var savedText = localStorage.getItem("savedText-" + rowId); //get item from the local storage by concatenating the savedText and rowId. This makes sure the unique id of the time-blow element is used to stop repeating.
+        if (savedText) { // if there is anything in local storage assign it to savedText
+            $(this).find(".description").val(savedText); //if savedText does have a value it is used to set the value of .description from html
+        }
+    });
+
+    $(".save-button").click(function(){ // select the class save-button and fun a new function when button is clicked
+        var textInput = $(this).siblings(".description").val(); // gets value of the class .description field in html
+        var rowId = $(this).closest('.time-block').attr('id'); // finds the closest element to the description with an id of time-block
+        localStorage.setItem("savedText-" + rowId, textInput); // uses this id to create a unique key to store input value in local storage and saves the text as savedText
+    });
+
+});
+
+
+//when i refreshed my page the text was being wiped so I need to modify it to retreive the information stored in the local storage and display it on the page
+// i checked the local storage and my text doesn't seem to be saving
+// i have managed to save the text to the local storage but now when i refresh the text copies across to all other boxes
